2c418d9cf099bc93542fe39207c3ebd9bcadb0c3
jaymspeights <jaymspeights@gmail.com>
2016-05-02 14:04:23 -0500

    created procedure for getting idea description

--- mainscript.js
+++ mainscript.js
@@ -1,11 +1,25 @@
+var id;
+
 function addIdea() {
+    var count = document.getElementById("counter");
     var node = document.createElement("LI");
+    node.setAttribute("id", "idea" + count.value);
+
     var buttonnode = document.createElement("BUTTON");
-    buttonnode.setAttribute("onclick", "showDetails()")
-    buttonnode.setAttribute("id", "idea" + document.getElementById("counter").value);
-    document.getElementById("counter").value += 1;
+    buttonnode.setAttribute("onclick", "showDetails(idea" + count.value + ")")
+
+
+    count.value = parseInt(count.value) + 1;
+
     var textnode = document.createTextNode("This is a idea");
     node.appendChild(buttonnode);
+
     buttonnode.appendChild(textnode);
     document.getElementById("listoftasks").appendChild(node);
+}
+
+function showDetails(listid) {
+    //console.log(listid);
+    var textnode = document.createTextNode("Description of idea");
+    listid.appendChild(textnode);
 }
